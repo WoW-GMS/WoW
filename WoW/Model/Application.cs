5baf84dac518fc53cbc97919a7cba9223e302666
westieh <theodor.kaalund@hotmail.com>
2025-04-24 22:37:08 +0200

    Applied .gitignore and removed tracked build/user files Tilføjet models for repo. Tilføjet en Apply page hvor man kan tilføje Applications Tilføjet en Ansøgninger side for at se ansøgninger. Tilføjet noget mockdata for at teste om man kan modificere fra siden.

--- WoW/Model/Application.cs
+++ WoW/Model/Application.cs
@@ -5,6 +5,8 @@
         public int ApplicationId { get; set; }
         public string CharacterName { get; set; }
         public string DiscordName { get; set; }
+        public Class Class { get; set; }
+        public Role Role { get; set; }
         public DateTime SubmissionDate { get; set; }
         public Member ProcessedBy { get; set; }
         public string Note { get; set; }
---
+++ WoW/Model/MemberRepo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using WoW.Model;
+
+namespace WoW.Model
+{
+    public class MemberRepo
+    {
+        public List<Member> Members { get; set; } = new();
+    }
+}
