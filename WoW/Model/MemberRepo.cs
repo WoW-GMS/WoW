55178053ee6fc6ca23923b1e882c9d4a84557d04
Simon Jensen <Simon.jensen.b@gmail.com>
2025-04-25 10:46:43 +0200

    CRUD til MemberRepo

--- WoW/Model/MemberRepo.cs
+++ WoW/Model/MemberRepo.cs
@@ -3,8 +3,54 @@ using WoW.Model;
 
 namespace WoW.Model
 {
-    public class MemberRepo
-    {
-        public List<Member> Members { get; set; } = new();
-    }
+	public class MemberRepo
+	{
+		private List<Member> _members = new List<Member>();
+
+		public Member AddMember(Member member)
+		{
+			_members.Add(member);
+			return member;
+		}
+
+		public Member? GetMember(int memberId)
+		{
+			foreach (Member member in _members)
+			{
+				if (member.MemberId == memberId) return member;
+			}
+			return null;
+		}
+
+		//UpdateMember gør brug af lambda
+		public Member? UpdateMember(int memberId, Member member)
+		{
+			var existingMember = _members.FirstOrDefault(m => m.MemberId == memberId);
+
+			if (existingMember != null)
+			{
+				existingMember.MemberId = memberId;
+				existingMember.Name = member.Name;
+				existingMember.Rank = member.Rank;
+				return existingMember;
+			}
+			return null;
+		}
+
+		public Member? DeleteMember(int memberId)
+		{
+			foreach (Member member in _members)
+			{
+				if (member.MemberId == memberId) _members.Remove(member);
+				return member;
+			}
+			return null;
+		}
+
+		public List<Member> GetMembers()
+		{
+			return new List<Member>(_members);
+		}
+	}
 }
+
